41a69ba575977f542b6d5304c7cfb8eebf60f285
profrog <ache159@naver.com>
2022-08-05 17:04:29 +0900

    Add files via upload

---
+++ cv_test.cpp
@@ -0,0 +1,15 @@
+namespace cv_bridge {
+
+class CvImage
+{
+public:
+  std_msgs::Header header;
+  std::string encoding;
+  cv::Mat image;
+};
+
+typedef boost::shared_ptr<CvImage> CvImagePtr;
+typedef boost::shared_ptr<CvImage const> CvImageConstPtr;
+
+}
+
